6aae689e54f88c2ec14922ae6f5d5a7fbbdb6350
Jascha <jascha@jaeh.at>
2014-06-03 20:46:16 +0200

    cleaned up gruntfile

--- GruntFile.js
+++ GruntFile.js
@@ -5,18 +5,12 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		stylus: {
 			compile: {
-				options: {
-					paths: ['css'],
-					// use embedurl('test.png') to trigger Data URI embedding
-					urlfunc: 'embedurl',
-				},
 				files: {
 					// compile and concat into single file
 					'css/main.css': ['css/reset.styl', 'css/main.styl', 'css/custom-widths.styl']
 				}
 			}
 		}
-		
 	});
 
 	// Load the plugin that provides the "jade" task.
