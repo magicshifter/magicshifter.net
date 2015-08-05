f21c9f727e052201c85536b3f15cc8da8cdfb237
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-05 06:06:30 +0200

    add header menu-toggle in mobile-view

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,40 @@
-var header = document.querySelector('header.main');
+(function() {
+  'use strict';
+  var header = document.querySelector('header.main');
 
-window.addEventListener('scroll', function(e) {
-  resizeHeader();
-});
+  window.addEventListener('scroll', function(e) {
+    resizeHeader();
+  });
 
-function resizeHeader() {
-  if (scrollY < 40 ) {
-    if (header.className.indexOf('big') < 0) {
-      header.classList.add('big');
+  function resizeHeader(cb) {
+    if (scrollY > 40 ) {
+      if (header.className.indexOf('small') < 0) {
+        header.classList.add('small');
+      }
+    } else if (header.className.indexOf('small') > -1) {
+      header.classList.remove('small');
     }
-  } else if (header.className.indexOf('big') > -1) {
-    header.classList.remove('big');
   }
-}
 
-resizeHeader();
+  resizeHeader();
+
+  setTimeout(function() {
+    document.body.classList.add('js');
+  }, 100);
+
+  var menuToggle = header.querySelector('.menu-toggle');
+  var nav = header.querySelector('nav.main');
+
+  menuToggle.addEventListener('click', menuToggleClickHandler);
+
+  function menuToggleClickHandler(e) {
+    nav.classList.toggle('show');
+
+    document.body.addEventListener('click', menuToggleClickHandler);
+
+    e.target.removeEventListener('click');
+    e.stopPropagation();
+    return false;
+  }
+
+})();
