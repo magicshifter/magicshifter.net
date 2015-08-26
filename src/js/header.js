985259fd97748ab3db5cc650514d2fb628bd3df3
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-26 14:30:14 +0200

    add menu item clickhandlers and scrollTo

--- src/js/header.js
+++ src/js/header.js
@@ -1,11 +1,6 @@
-const header = document.querySelector('header.main');
-
-window.addEventListener('scroll', resizeHeader);
-
-resizeHeader();
-
 setTimeout(() => document.body.classList.add('js'), 100);
 
+const header = document.querySelector('header.main');
 const menuToggle = header.querySelector('.menu-toggle');
 const nav = header.querySelector('nav.main');
 
@@ -23,8 +18,39 @@ function menuToggleClickHandler(e) {
   e.currentTarget.removeEventListener('click', menuToggleClickHandler);
 
   e.stopPropagation();
+  e.preventDefault();
   return false;
 }
+
+const menuItems = nav.querySelectorAll('a');
+const topOffsets = {};
+
+Object.keys(menuItems).forEach((key) => {
+  const item = menuItems[key];
+  addMenuItemListener(item);
+});
+
+function addMenuItemListener(item) {
+  if (typeof item.addEventListener === 'function') {
+    if (item.href.indexOf('#') > -1) {
+      const selector = item.href.split('#')[1];
+      topOffsets[selector] = document.querySelector(`#${selector}`).offsetTop;
+      item.addEventListener('click', menuItemClickHandler);
+    }
+  }
+}
+
+function menuItemClickHandler(e) {
+  const href = e.target.href;
+  const selector = href.split('#')[1];
+  const topOffset = topOffsets[selector];
+  window.scrollTo(0, topOffset);
+  window.location = href;
+}
+
+window.addEventListener('scroll', resizeHeader);
+
+resizeHeader();
 
 function resizeHeader(cb) {
   if (scrollY > 40) {
