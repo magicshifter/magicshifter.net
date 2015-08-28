3c6724d7068317a7dc6e4ea45ff67d55e4884edc
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-28 17:43:54 +0200

    update js files to use new magicbuilder tasks

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import './ga';
-
-import './header';
-import './menutoggle';
-import './router';
-
-setTimeout(() => document.body.classList.add('js'), 1);
---
+++ src/js/index/index.js
@@ -0,0 +1,6 @@
+setTimeout(() => document.body.classList.add('js'), 1);
+
+import './ga';
+import './header';
+import './menutoggle';
+import './router';
