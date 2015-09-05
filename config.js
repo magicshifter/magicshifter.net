adef32820af6e096d5bfa920e170c43186560c90
Jascha Ehrenreich <jascha@jaeh.at>
2015-09-05 02:58:07 +0200

    rename social icon variables

--- config.js
+++ config.js
@@ -24,53 +24,53 @@ var dirs = {
 
 var socialAccounts = [
   {
-      link: 'http://blog.magicshifter.net/'
-    , img: '/img/social-icons/soup.png'
-    , alt: 'soup'
-    , title: 'repost Magicshifter on soup'
-    , visible: true
+    href: 'http://blog.magicshifter.net/',
+    src: '/img/social-icons/soup.png',
+    alt: 'soup',
+    title: 'repost Magicshifter on soup',
+    visible: true,
   },
   {
-      link: 'https://www.flickr.com/photos/wizard23/sets/72157632220498947/detail/'
-    , img: '/img/social-icons/flickr.png'
-    , alt: 'MagicShifter picture blog on Flickr'
-    , title: 'MagicShifter picture blog on Flickr'
-    , visible: true
+    href: 'https://www.flickr.com/photos/wizard23/sets/72157632220498947/detail/',
+    src: '/img/social-icons/flickr.png',
+    alt: 'MagicShifter picture blog on Flickr',
+    title: 'MagicShifter picture blog on Flickr',
+    visible: true,
   },
   {
-      link: 'http://www.reddit.com/r/MagicShifter/'
-    , img: '/img/social-icons/reddit.png'
-    , alt: 'MagicShifter Forum on Reddit'
-    , title: 'MagicShifter Forum on Reddit'
-    , visible: true
+    href: 'http://www.reddit.com/r/MagicShifter/',
+    src: '/img/social-icons/reddit.png',
+    alt: 'MagicShifter Forum on Reddit',
+    title: 'MagicShifter Forum on Reddit',
+    visible: true,
   },
   {
-      link: 'https://github.com/magicshifter'
-    , img: '/img/social-icons/github.png'
-    , alt: 'Our Github Organisation'
-    , title: 'We <3 the Octocat'
-    , visible: true
+    href: 'https://github.com/magicshifter',
+    src: '/img/social-icons/github.png',
+    alt: 'Our Github Organisation',
+    title: 'We <3 the Octocat',
+    visible: true,
   },
   {
-      link: 'https://twitter.com/magicshifter'
-    , img: '/img/social-icons/twitter.png'
-    , alt: 'twitter'
-    , title: 'follow Magicshifter on twitter'
-    , visible: true
+    href: 'https://twitter.com/magicshifter',
+    src: '/img/social-icons/twitter.png',
+    alt: 'twitter',
+    title: 'follow Magicshifter on twitter',
+    visible: true,
   },
   {
-      link: 'https://www.facebook.com/magicshifter/'
-    , img: '/img/social-icons/facebook.png'
-    , alt: 'facebook'
-    , title: 'like magicshifter on facebook'
-    , visible: true
+    href: 'https://www.facebook.com/magicshifter/',
+    src: '/img/social-icons/facebook.png',
+    alt: 'facebook',
+    title: 'like magicshifter on facebook',
+    visible: true,
   },
   {
-      link: 'https://flattr.com/submit/auto?user_id=wizard23&url=http%3A%2F%2Fmagicshifter.net'
-    , img: '/img/social-icons/flattr.png'
-    , alt: 'Flattr this'
-    , title: 'Flattr this'
-    , visible: true
+    href: 'https://flattr.com/submit/auto?user_id=wizard23&url=http%3A%2F%2Fmagicshifter.net',
+    src: '/img/social-icons/flattr.png',
+    alt: 'Flattr this',
+    title: 'Flattr this',
+    visible: true,
   },
 ];
 
