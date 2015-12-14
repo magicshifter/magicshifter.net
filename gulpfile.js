dbe8525f2f3ee04aab7ad4e782b9288cae75995c
Jascha Ehrenreich <jascha@jaeh.at>
2015-12-14 17:59:07 +0100

    prepare for magic

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp');
-var config = require('./config');
-var builder = require('builder');
-
-builder(gulp, config);
--- js/v1/magic/index.js
+++ js/v1/magic/index.js
@@ -1,4 +1,3 @@
-import './libs/jquery-1.9.1.min.js';
 import './libs/FileSaver';
 import './libs/libgif.js';
 import './magicbitmap';
